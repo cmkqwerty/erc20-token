a26ad3dfb5ca4192aeb74e0e8049b04ee73975a6
cmkqwerty <56502922+cmkqwerty@users.noreply.github.com>
2023-06-24 19:25:20 +0300

    [feat] Additional Features

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -6,8 +6,18 @@ contract ERC20 {
     string public name;
     string public symbol;
 
+    bool internal locked;
+
+    modifier noReentrant() {
+        require(!locked, "ERC20: No re-entrancy.");
+        locked = true;
+        _;
+        locked = false;
+    }
+
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Redeem(address indexed from, uint256 amount);
 
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance; 
@@ -27,7 +37,7 @@ contract ERC20 {
         return _transfer(msg.sender, recipient, amount);
     } 
 
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
         uint256 currentAllowance = allowance[sender][msg.sender];
 
         require(currentAllowance >= amount, "ERC20: Amount exceeded the allowance value.");
@@ -69,6 +79,37 @@ contract ERC20 {
         balanceOf[to] += amount;
 
         emit Transfer(address(0), to, amount);
+    }
+
+    function deposit() external payable {
+        uint256 tokensToMint = msg.value;
+
+        _mint(msg.sender, tokensToMint);
+    }
+
+    function redeem(uint256 amount) external noReentrant{
+        require(allowance[msg.sender][address(this)] >= amount, "ERC20: Redeem amount exceeded.");
+
+        transferFrom(msg.sender, address(this), amount);
+
+        _burn(amount);
+
+        uint256 etherValue = amount;
+
+        (bool sent,) = payable(msg.sender).call{value: amount}("");
+
+        require(sent, "ERC20: Failed to send Ether.");
+
+        emit Redeem(msg.sender, etherValue);
+    }
+
+    function _burn(uint256 amount) private {
+        require(balanceOf[address(this)] >= amount, "ERC20: Insufficient balance for burning.");
+
+        balanceOf[address(this)] -= amount;
+        totalSupply -= amount;
+
+        emit Transfer(address(this), address(0), amount);
     }
 
 }
